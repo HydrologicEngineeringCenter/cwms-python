ed2c5303c9e342ab18231147ef95554063bf879a
Adam Korynta <adam@rmanet.com>
2024-02-09 15:25:47 -0800

    create default factory methods pointing to CWBI Test

--- CWMS/__init__.py
+++ CWMS/__init__.py
@@ -9,3 +9,32 @@ try:
     __version__ = version('cwms-python')
 except PackageNotFoundError:
     __version__ = 'version-unknown'
+
+__CWBI_URL = "https://cwms-data-test.cwbi.us/cwms-data/"
+
+def cwms_ts() -> CwmsTs:
+    """
+    Creates an instance of CwmsTs using a CwmsApiSession with the CWBI URL.
+
+    Returns:
+    CwmsTs: An instance of CwmsTs.
+    """
+    return CwmsTs(CwmsApiSession(__CWBI_URL))
+
+def cwms_loc() -> CwmsLoc:
+    """
+    Creates an instance of CwmsTs using a CwmsApiSession with the CWBI URL.
+
+    Returns:
+    CwmsTs: An instance of CwmsTs.
+    """
+    return CwmsLoc(CwmsApiSession(__CWBI_URL))
+
+def cwms_level() -> CwmsLevel:
+    """
+    Creates an instance of CwmsTs using a CwmsApiSession with the CWBI URL.
+
+    Returns:
+    CwmsTs: An instance of CwmsTs.
+    """
+    return CwmsLevel(CwmsApiSession(__CWBI_URL))
---
+++ examples/examples.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2024
+#  United States Army Corps of Engineers - Hydrologic Engineering Center (USACE/HEC)
+#  All Rights Reserved.  USACE PROPRIETARY/CONFIDENTIAL.
+#  Source may not be released without written approval from HEC
+
+
+import CWMS
+
+CWMS.cwms_ts().retrieve_ts_json("ARBU.Elev.Inst.1Hour.0.Ccp-Rev", "SWT")
