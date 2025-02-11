42a72b54e7abd2d4f2b9dcbeba4a720e93015f98
Charles Graham SWT <krowven@gmail.com>
2025-02-11 13:50:20 -0600

    Add turbines_with_name test,
    Add delete turbines with name

--- tests/turbines/turbines_test.py
+++ tests/turbines/turbines_test.py
@@ -9,12 +9,18 @@ from tests._test_utils import read_resource_file
 
 _MOCK_ROOT = "https://mockwebserver.cwms.gov"
 _TURBINES = read_resource_file("turbines.json")
+_TURBINES_NAME = read_resource_file("turbines_name.json")
 _TURBINES_OFFICE_NAME = read_resource_file("turbines_office_name.json")
 
 
 @pytest.fixture(autouse=True)
 def init_session():
     cwms.api.init_session(api_root=_MOCK_ROOT)
+
+
+# ==========================================================================
+#                             GET CWMS TURBINES
+# ==========================================================================
 
 
 def test_get_projects_turbines(requests_mock):
@@ -117,6 +123,47 @@ def test_get_projects_turbines(requests_mock):
     assert actual_values == expected_values
 
 
+def get_projects_turbines_with_name(requests_mock):
+    requests_mock.get(
+        f"{_MOCK_ROOT}/projects/turbines?name=KEYS&office=SWT",
+        json=_TURBINES_NAME,
+    )
+
+    project_id = "KEYS"
+    office = "SWT"
+
+    data = turbines.get_projects_turbines(office=office, project_id=project_id)
+    expected_columns = [
+        "project-id.office-id",
+        "project-id.name",
+        "location.office-id",
+        "location.name",
+        "location.latitude",
+        "location.longitude",
+        "location.active",
+        "location.public-name",
+        "location.long-name",
+        "location.timezone-name",
+        "location.location-kind",
+        "location.nation",
+        "location.state-initial",
+        "location.county-name",
+        "location.nearest-city",
+        "location.horizontal-datum",
+        "location.published-longitude",
+        "location.published-latitude",
+        "location.vertical-datum",
+        "location.elevation",
+        "location.bounding-office-id",
+        "location.elevation-units",
+    ]
+
+    expected_values = [["SWT", "KEYS", "SWT"]]
+    assert list(data.df.columns) == expected_columns
+    assert data.df.shape == (1, len(expected_columns))
+    assert data.df.to_numpy().tolist() == expected_values
+
+
 def test_get_projects_turbines_with_office_with_name(requests_mock):
     requests_mock.get(
         f"{_MOCK_ROOT}/projects/SWT/KEYS/turbines", json=_TURBINES_OFFICE_NAME
@@ -209,6 +256,11 @@ def test_get_projects_turbines_with_office_with_name(requests_mock):
     assert data.df.to_numpy().tolist() == expected_values
 
 
+# ==========================================================================
+#                             POST CWMS TURBINES
+# ==========================================================================
+
+
 def test_store_projects_turbines(requests_mock):
     requests_mock.post(f"{_MOCK_ROOT}/projects/turbines?fail-if-exists=false")
 
@@ -225,5 +277,43 @@ def test_store_projects_turbines_with_office_with_name(requests_mock):
     turbines.store_projects_turbines_with_office_with_name(
         data=_TURBINES_OFFICE_NAME, office="SWT", name="KEYS", override_protection=False
     )
+    assert requests_mock.called
+    assert requests_mock.call_count == 1
+
+
+# ==========================================================================
+#                             DELETE CWMS TURBINES
+# ==========================================================================
+
+
+def test_delete_projects_turbines_with_name(requests_mock):
+    requests_mock.delete(
+        f"{_MOCK_ROOT}/projects/turbines/KEYS-Turbine1?office=SWT&method=DELETE_ALL"
+    )
+
+    name = "KEYS-Turbine1"
+    office = "SWT"
+    method = "DELETE_ALL"
+
+    turbines.delete_projects_turbines_with_name(name=name, office=office, method=method)
+
+    assert requests_mock.called
+    assert requests_mock.call_count == 1
+
+
+def test_delete_projects_turbines_with_office_with_name(requests_mock):
+    requests_mock.delete(f"{_MOCK_ROOT}/projects/SWT/KEYS/turbines")
+
+    office = "SWT"
+    name = "KEYS"
+
+    turbines.delete_projects_turbines_with_office_with_name(
+        office=office,
+        name=name,
+        override_protection=False,
+        begin=datetime(2024, 1, 1),
+        end=datetime(2024, 12, 31),
+    )
+
     assert requests_mock.called
     assert requests_mock.call_count == 1
