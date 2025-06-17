9004895fa52d65091192d19690100fe92b58d610
Charles Graham SWT <krowven@gmail.com>
2025-06-17 16:27:21 -0500

    Remove unused imports

--- cwms/projects/water_supply/accounting.py
+++ cwms/projects/water_supply/accounting.py
@@ -2,8 +2,6 @@
 #  United States Army Corps of Engineers - Hydrologic Engineering Center (USACE/HEC)
 #  All Rights Reserved.  USACE PROPRIETARY/CONFIDENTIAL.
 #  Source may not be released without written approval from HEC
-from typing import Optional
-
 import cwms.api as api
 from cwms.cwms_types import JSON, Data
 
