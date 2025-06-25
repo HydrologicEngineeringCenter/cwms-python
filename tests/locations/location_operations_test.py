f0df764489ebe7bd76178b19eeca227a117efbd7
Oskar Hurst <oskar.a.hurst@usace.army.mil>
2025-06-25 00:16:00 +0000

    fix location operations test

--- tests/locations/location_operations_test.py
+++ tests/locations/location_operations_test.py
@@ -8,12 +8,12 @@ import cwms.api
 
 @pytest.fixture(autouse=True)
 def init_session():
-    cwms.api.init_session(api_root='localhost:8081/cwms-data', api_key='1234567890abcdef1234567890abcdef')
+    cwms.api.init_session(api_root='http://localhost:8081/cwms-data/', api_key='1234567890abcdef1234567890abcdef')
 
 def test_get_location_operations():
     """
     Test the retrieval of location operations from the CWMS API.
     """
     loc_cat = cwms.get_locations_catalog(office_id='SPK')
-
-    assert(len(loc_cat)==0)  # Assuming no locations are present for SPK office in the test environment
+    
+    assert(len(loc_cat.df)==0)  # Assuming no locations are present for SPK office in the test environment
