b491403a6ec350cbef031a202196a447025510d0
msweier <mitchell.s.weier@usace.army.mil>
2025-09-10 12:51:51 -0500

    fix typing

--- cwms/catalog/catalog.py
+++ cwms/catalog/catalog.py
@@ -134,7 +134,7 @@ def get_timeseries_catalog(
     return Data(response, selector="entries")
 
 
-def get_ts_extents(ts_id=str, office_id=str):
+def get_ts_extents(ts_id: str, office_id: str):
     """Retrieves earliest extent, latest extent, and last update via cwms.get_timeseries_catalog
 
     Parameters
