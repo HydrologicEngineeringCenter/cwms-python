e27837ce50adadb204dbdf158e16815aa375a940
Daniel Osborne <Daniel.T.Osborne@usace.army.mil>
2026-03-31 09:13:14 -0700

    Pass the ID in the query if it has invalid chars. (#242)
    * Pass the ID in the query if it has invalid chars.
    
    * Changes based on test findings.
    
    * Add tests.
    
    * Fix for test failures.
    
    * Adjusted CDA image to develop-nightly.
    
    Per @MikeNeilson, latest-dev isn't used anymore in cwms-data-api.
    
    * Format change for sonarqube.
    
    * Fix typo.
    
    * Fix for SonarQube findings.
    
    * Bump docker compose to match latest CDA @MikeNeilson
    
    * Attempt to make Clob/Blob tests DRY
    
    * Fix the formatting for catalog_resources tests
    
    * Fix lob formatting
    
    ---------
    
    Co-authored-by: Charles Graham <charles.r.graham@usace.army.mil>
    Co-authored-by: Charles Graham SWT <krowven@gmail.com>
    Co-authored-by: Eric Novotny <eric.v.novotny@usace.army.mil>

--- cwms/catalog/blobs.py
+++ cwms/catalog/blobs.py
@@ -1,9 +1,9 @@
 import base64
-from typing import Optional
+from typing import Any, Optional
 
 import cwms.api as api
 from cwms.cwms_types import JSON, Data
-from cwms.utils.checks import is_base64
+from cwms.utils.checks import has_invalid_chars, is_base64
 
 STORE_DICT = """data = {
     "office-id": "SWT",
@@ -13,6 +13,8 @@ STORE_DICT = """data = {
     "value": "STRING of content or BASE64_ENCODED_STRING"
 }
 """
+
+IGNORED_ID = "ignored"
 
 
 def get_blob(blob_id: str, office_id: str) -> str:
@@ -29,8 +31,13 @@ def get_blob(blob_id: str, office_id: str) -> str:
         str: the value returned based on the content-type it was stored with as a string
     """
 
-    endpoint = f"blobs/{blob_id}"
-    params = {"office": office_id}
+    params: dict[str, Any] = {}
+    if has_invalid_chars(blob_id):
+        endpoint = f"blobs/{IGNORED_ID}"
+        params["blob-id"] = blob_id
+    else:
+        endpoint = f"blobs/{blob_id}"
+    params["office"] = office_id
     response = api.get(endpoint, params, api_version=1)
     return str(response)
 
@@ -107,8 +114,13 @@ def delete_blob(blob_id: str, office_id: str) -> None:
         None
     """
 
-    endpoint = f"blobs/{blob_id}"
-    params = {"office": office_id}
+    params: dict[str, Any] = {}
+    if has_invalid_chars(blob_id):
+        endpoint = f"blobs/{IGNORED_ID}"
+        params["blob-id"] = blob_id
+    else:
+        endpoint = f"blobs/{blob_id}"
+    params["office"] = office_id
     return api.delete(endpoint, params, api_version=1)
 
 
@@ -143,6 +155,11 @@ def update_blob(data: JSON, fail_if_not_exists: Optional[bool] = True) -> None:
 
     blob_id = data.get("id", "").upper()
 
-    endpoint = f"blobs/{blob_id}"
-    params = {"fail-if-not-exists": fail_if_not_exists}
+    params: dict[str, Any] = {}
+    if has_invalid_chars(blob_id):
+        endpoint = f"blobs/{IGNORED_ID}"
+        params["blob-id"] = blob_id
+    else:
+        endpoint = f"blobs/{blob_id}"
+    params["fail-if-not-exists"] = fail_if_not_exists
     return api.patch(endpoint, data, params, api_version=1)
