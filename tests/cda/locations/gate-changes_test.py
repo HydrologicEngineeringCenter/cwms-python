a9be0b277736784b959ac2b422c980a308260fbc
zack-rma <zack@rmanet.com>
2026-08-26 12:25:48 -0700

    Fixed api versioning for projects endpoints. Updated tests for gate changes

--- tests/cda/locations/gate-changes_test.py
+++ tests/cda/locations/gate-changes_test.py
@@ -3,12 +3,20 @@ import pytest
 import cwms
 import cwms.locations.gate_changes as gc
 import cwms.locations.physical_locations as pl
+import cwms.projects.projects as proj
 
 TEST_OFFICE = "SPK"
 TEST_PROJECT_ID = "BIGH"
 TEST_LOCATION_ID = "BIGH-CG100"
 START = "2024-01-01T00:00:00Z"
 END = "2024-01-02T00:00:00Z"
+PUMP_LOCATION_ID = "Sac River-Pump 1"
+PUMP_LOCATION_ID2 = "Sac River-Pump 2"
+PUBLIC_NAME = "Test Public Pump Name"
+LONG_NAME = "Test Long Name"
+LOCATION_TYPE = "Test Location Type"
+DESCRIPTION = "Test Description"
+MAP_LABEL = "Test Map Label"
 
 TEST_PROJECT_LOCATION = {
     "name": TEST_PROJECT_ID,
@@ -42,46 +50,136 @@ TEST_LOCATION = {
     "nation": "US",
 }
 
-GATE_CHANGE = {
-    "type": "gate-change",
-    "project-id": {"office-id": TEST_OFFICE, "name": TEST_PROJECT_ID},
-    "change-date": 1704096000000,
-    "pool-elevation": 3.0,
-    "protected": True,
-    "discharge-computation-type": {
+PUMP_LOCATION1 = {
+    "office-id": TEST_OFFICE,
+    "name": PUMP_LOCATION_ID,
+    "latitude": 0,
+    "longitude": 0,
+    "active": True,
+    "public-name": PUBLIC_NAME,
+    "long-name": LONG_NAME,
+    "description": DESCRIPTION,
+    "timezone-name": "UTC",
+    "location-type": LOCATION_TYPE,
+    "location-kind": "PUMP",
+    "nation": "US",
+    "state-initial": "NV",
+    "county-name": "Clark",
+    "nearest-city": "Sparks",
+    "horizontal-datum": "WGS84",
+    "published-longitude": 0,
+    "published-latitude": 0,
+    "vertical-datum": "NGVD29",
+    "elevation": 150,
+    "map-label": MAP_LABEL,
+    "bounding-office-id": TEST_OFFICE,
+    "elevation-units": "m",
+}
+
+PUMP_LOCATION2 = {
+    "office-id": TEST_OFFICE,
+    "name": PUMP_LOCATION_ID2,
+    "latitude": 0,
+    "longitude": 0,
+    "active": True,
+    "public-name": PUBLIC_NAME,
+    "long-name": LONG_NAME,
+    "description": DESCRIPTION,
+    "timezone-name": "UTC",
+    "location-type": LOCATION_TYPE,
+    "location-kind": "PUMP",
+    "nation": "US",
+    "state-initial": "NV",
+    "county-name": "Clark",
+    "nearest-city": "Sparks",
+    "horizontal-datum": "WGS84",
+    "published-longitude": 0,
+    "published-latitude": 0,
+    "vertical-datum": "NGVD29",
+    "elevation": 150,
+    "map-label": MAP_LABEL,
+    "bounding-office-id": TEST_OFFICE,
+    "elevation-units": "m",
+}
+
+GATE_CHANGE = [
+    {
+        "type": "gate-change",
+        "project-id": {"office-id": TEST_OFFICE, "name": TEST_PROJECT_ID},
+        "change-date": 1704096000000,
+        "pool-elevation": 3.0,
+        "protected": True,
+        "discharge-computation-type": {
+            "office-id": TEST_OFFICE,
+            "display-value": "A",
+            "tooltip": "Adjusted by an automated method",
+            "active": True,
+        },
+        "reason-type": {
+            "office-id": TEST_OFFICE,
+            "display-value": "O",
+            "tooltip": "Other release",
+            "active": True,
+        },
+        "notes": "Test notes",
+        "new-total-discharge-override": 1.0,
+        "old-total-discharge-override": 2.0,
+        "discharge-units": "cfs",
+        "tailwater-elevation": 4.0,
+        "elevation-units": "ft",
+        "settings": [
+            {
+                "type": "gate-setting",
+                "location-id": {"office-id": TEST_OFFICE, "name": TEST_LOCATION_ID},
+                "opening": 10.0,
+                "opening-parameter": "Opening",
+                "opening-units": "ft",
+                "invert-elevation": 20.0,
+            }
+        ],
+    }
+]
+
+PROJECT = {
+    "location": {
         "office-id": TEST_OFFICE,
-        "display-value": "A",
-        "tooltip": "Adjusted by an automated method",
-        "active": True,
+        "name": TEST_PROJECT_ID,
+        "timezone-name": "UTC",
     },
-    "reason-type": {
+    "federal-cost": 100.0,
+    "non-federal-cost": 50.0,
+    "cost-year": 1717282800000,
+    "cost-unit": "$",
+    "federal-o-and-m-cost": 10.0,
+    "non-federal-o-and-m-cost": 5.0,
+    "authorizing-law": "Authorizing Law",
+    "project-owner": "Project Owner",
+    "hydropower-desc": "Hydropower Description",
+    "sedimentation-desc": "Sedimentation Description",
+    "downstream-urban-desc": "Downstream Urban Description",
+    "bank-full-capacity-desc": "Bank Full Capacity Description",
+    "pump-back-location": {
         "office-id": TEST_OFFICE,
-        "display-value": "O",
-        "tooltip": "Other release",
-        "active": True,
+        "name": PUMP_LOCATION,
+        "timezone-name": "UTC",
     },
-    "notes": "Test notes",
-    "new-total-discharge-override": 1.0,
-    "old-total-discharge-override": 2.0,
-    "discharge-units": "cfs",
-    "tailwater-elevation": 4.0,
-    "elevation-units": "ft",
-    "settings": [
-        {
-            "type": "gate-setting",
-            "location-id": {"office-id": TEST_OFFICE, "name": TEST_LOCATION_ID},
-            "opening": 10.0,
-            "opening-parameter": "Opening",
-            "opening-units": "ft",
-            "invert-elevation": 20.0,
-        }
-    ],
+    "near-gage-location": {
+        "office-id": "SPK",
+        "name": PUMP_LOCATION2,
+        "timezone-name": "UTC",
+    },
+    "yield-time-frame-start": 1717282800000,
+    "yield-time-frame-end": 1717308000000,
+    "project-remarks": "Remarks",
 }
 
 
 def _cleanup():
+    proj.delete_project(TEST_PROJECT_ID, TEST_OFFICE)
     pl.delete_location(TEST_PROJECT_LOCATION, TEST_OFFICE)
     pl.delete_location(TEST_LOCATION, TEST_OFFICE)
+    pl.delete_location(PUMP_LOCATION1, TEST_OFFICE)
+    pl.delete_location(PUMP_LOCATION2, TEST_OFFICE)
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -91,8 +189,11 @@ def setup_data():
     except Exception:
         pass
 
+    pl.store_location(PUMP_LOCATION1, False)
+    pl.store_location(PUMP_LOCATION2, False)
     pl.store_location(TEST_PROJECT_LOCATION, False)
     pl.store_location(TEST_LOCATION, False)
+    proj.store_project(PROJECT, False)
 
 
 @pytest.fixture(autouse=True)
@@ -146,4 +247,4 @@ def test_delete_gate_change():
     data = gc.get_all_gate_changes(
         TEST_OFFICE, new_loc, START, END, True, True, "SI", 10
     )
-    assert len(data) == 0
+    assert len(data.json) == 0
